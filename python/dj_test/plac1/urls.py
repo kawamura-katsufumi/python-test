8ab30e544f3ac7a83e47d7320c6eba17a0dc1043
kawamura <lailai666@hotmail.com>
2022-02-05 00:48:53 +0900

    error盛りだくさん

--- python/dj_test/plac1/urls.py
+++ python/dj_test/plac1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index,about,moto
 
 urlpatterns = [
-    path('', index,name="index"),
+    path('',moto,name="moto"),
+    path('plac1/index/', index,name="index"),
+    path('plac1/about/', about,name="about"),
 ]
--- python/dj_test/plac1/views.py
+++ python/dj_test/plac1/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def moto(request):
+    return render(request,"plac1/moto.html")
+
 def index(request):
-    return render(request,"index.html")
+    return render(request,"plac1/index.html")
+
+def about(request):
+    return render(request,"plac1/about.html")
