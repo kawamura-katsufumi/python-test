17ddefa8c7f969596112821745fa32148b8ec3c4
kawamura <lailai666@hotmail.com>
2022-02-05 22:00:16 +0900

    111

--- python/dj_test/plac1/views.py
+++ python/dj_test/plac1/views.py
@@ -4,7 +4,8 @@ def moto(request):
     return render(request,"plac1/moto.html")
 
 def index(request):
-    return render(request,"plac1/index.html")
+    username="川村"
+    return render(request,"plac1/index.html",{"username":username})
 
 def about(request):
     return render(request,"plac1/about.html")
