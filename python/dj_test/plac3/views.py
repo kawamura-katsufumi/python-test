574db89f5878ef666978cad5a6ce53591bf61318
p1-kawamura <kawamura@p1-intl.com>
2022-02-07 16:43:49 +0900

    jikken

--- python/dj_test/plac3/views.py
+++ python/dj_test/plac3/views.py
@@ -8,9 +8,23 @@ def omote(request,num=1):
     pages=Paginator(items, 4)
     data=pages.get_page(num)
 
+    ###　実験　###
+    dic={}
+    jdata1=Ura.objects.all()
+    jdata2=Ura.objects.distinct().values_list("club",flat=True)
+    for item in jdata2:
+        a=[]
+        for i in jdata1:
+            if str(item) == str(i):
+                b=str(i.name2)
+                a.append(b)
+        a=" , ".join(a)
+        dic[item]=a
+    ###　ここまで　###
+
     params={
         "data":data,
-        "clubs":Ura.objects.all(),
+        "data2":dic,
         "form1":Omoteform(),
         "form2":Uraform(),
             }
