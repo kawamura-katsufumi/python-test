434a466eaf6f3756f0fca9ad1871f950d64c0a9d
p1-kawamura <kawamura@p1-intl.com>
2022-02-16 11:02:39 +0900

    com2.16.11.01

--- python/dj_test/plac4/urls.py
+++ python/dj_test/plac4/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index,users_detail
+from .views import index,users_detail,signup
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('index/',index,name="index"),
     path('users/<int:pk>/',users_detail,name="users_detail"),
-    path('login/',auth_views.LoginView.as_view(template_name="plac4/login.html"),name="longin"),
+    path('login/',auth_views.LoginView.as_view(template_name="plac4/login.html"),name="login"),
     path('logout/',auth_views.LogoutView.as_view(),name="logout"),
+    path('signup',signup,name="signup"),
 ]
--- python/dj_test/plac4/views.py
+++ python/dj_test/plac4/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from .models import Photo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from .forms import PhotoForm
 
 
 def index(request):
@@ -11,3 +15,31 @@ def users_detail(request,pk):
     user=User.objects.get(pk=pk)
     photos=user.photo_set.all().order_by("-created_at")
     return render(request,"plac4/users_detail.html",{"user":user,"photos":photos})
+
+def signup(request):
+    if request.method =="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            input_username=request.POST["username"]
+            input_password=request.POST["password1"]
+            new_user=authenticate(username=input_username,password=input_password)
+            if new_user is not None:
+                login(request,new_user)
+                return redirect("users_detail",pk=new_user.pk)
+    else:
+        form=UserCreationForm()
+    return render(request,"plac4/signup.html",{"form":form})
+
+@login_required
+def photos_new(request):
+    if request.POST == "POST":
+        form=PhotoForm(request.POST,request.FILES)
+        if form.is_valid():
+            photo=form.save(commit=False)
+            photo.user=request.user
+            photo.save()
+        return redirect("users_detail",pk=request.user.pk)
+    else:
+        form=PhotoForm()
+    return render(request,"plac4/photos_new.html",{"form":form})
