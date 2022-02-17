5ae65b78bdae5991b75fccb08fac114d4e3dde29
kawamura <lailai666@hotmail.com>
2022-02-17 22:34:32 +0900

    nt 不明

--- python/dj_test/plac5/views.py
+++ python/dj_test/plac5/views.py
@@ -7,19 +7,21 @@ def index(request):
 
 def add(request,pk):
 
+    # request.session.modified = True
+
     # del request.session['sample']
 
     item=Session.objects.get(pk=pk)
 
     if "sample" in request.session:
         
-        print(request.session["sample"])
+        # print(request.session["sample"])
 
-        # print("あり")
-        if item.hinban not in request.session["sample"]:
+        # # print("あり")
+        # if item.hinban not in request.session["sample"]:
 
-            # print(request.session["sample"])
-            request.session["sample"][item.hinban]=1
+        #     # print(request.session["sample"])
+        request.session["sample"][item.hinban]="1"
         
         
         print(request.session["sample"])
@@ -28,15 +30,15 @@ def add(request,pk):
 
     else:
         print("なし")
-        request.session["sample"]={item.hinban:1}
+        request.session["sample"]=item.hinban
         
     return redirect("index")
 
 
 
-def get_context_data(self, **kwargs):
-        # セッションを追加する
-        self.request.session['add_session'] = 'セッション追加'
-        self.request.session['array_session'] = dict(a='配列も', b='セッションに保管できます')
+# def get_context_data(self, **kwargs):
+#         # セッションを追加する
+#         self.request.session['add_session'] = 'セッション追加'
+#         self.request.session['array_session'] = dict(a='配列も', b='セッションに保管できます')
         
-        return redirect("index")
+#         return redirect("index")
