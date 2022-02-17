a364c8eb5f726da60cd1025e509685d04b149136
p1-kawamura <kawamura@p1-intl.com>
2022-02-17 17:48:09 +0900

    com セッション保存できない

--- python/dj_test/plac5/views.py
+++ python/dj_test/plac5/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Session
 
 def index(request):
     items=Session.objects.all()
     return render(request,"plac5/index.html",{"items":items})
+
+def add(request,pk):
+
+    # del request.session['sample']
+
+    item=Session.objects.get(pk=pk)
+
+    if "sample" in request.session:
+        
+        print(request.session["sample"])
+
+        # print("あり")
+        if item.hinban not in request.session["sample"]:
+
+            # print(request.session["sample"])
+            request.session["sample"][item.hinban]=1
+        
+        
+        print(request.session["sample"])
+
+
+
+    else:
+        print("なし")
+        request.session["sample"]={item.hinban:1}
+        
+    return redirect("index")
+
+
+
+def get_context_data(self, **kwargs):
+        # セッションを追加する
+        self.request.session['add_session'] = 'セッション追加'
+        self.request.session['array_session'] = dict(a='配列も', b='セッションに保管できます')
+        
+        return redirect("index")
