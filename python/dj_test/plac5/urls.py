acf70472b972ad8ab124565c8bb446616646cc2f
p1-kawamura <kawamura@p1-intl.com>
2022-02-18 10:20:53 +0900

    com セッション完了

--- python/dj_test/plac5/urls.py
+++ python/dj_test/plac5/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index,add
+from .views import index,add,delete
 
 urlpatterns = [
     path('index/', index, name="index"),
     path('add/<int:pk>', add, name="add"),
+    path('delete/', delete, name="delete"),
 ]
--- python/dj_test/plac5/views.py
+++ python/dj_test/plac5/views.py
@@ -5,11 +5,10 @@ def index(request):
     items=Session.objects.all()
     return render(request,"plac5/index.html",{"items":items})
 
+
 def add(request,pk):
 
     # request.session.modified = True
-
-    # del request.session['sample']
 
     item=Session.objects.get(pk=pk)
 
@@ -21,7 +20,7 @@ def add(request,pk):
         # if item.hinban not in request.session["sample"]:
 
         #     # print(request.session["sample"])
-        request.session["sample"]=1
+        request.session["sample"][item.hinban]=1
         
         
         print(request.session["sample"])
@@ -30,15 +29,14 @@ def add(request,pk):
 
     else:
         print("なし")
-        request.session["sample"]=item.hinban
+        request.session["sample"]={item.hinban:1}
+
+        print(request.session["sample"])
         
     return redirect("index")
 
 
-
-# def get_context_data(self, **kwargs):
-#         # セッションを追加する
-#         self.request.session['add_session'] = 'セッション追加'
-#         self.request.session['array_session'] = dict(a='配列も', b='セッションに保管できます')
-        
-#         return redirect("index")
+def delete(request):
+    del request.session['sample']
+    print("削除OK")
+    return redirect("index")
