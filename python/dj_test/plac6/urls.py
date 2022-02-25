34012191d99db4d8abd87c62633fbd6d337d1472
kawamura <lailai666@hotmail.com>
2022-02-26 01:21:02 +0900

    note 今日はもう無理　酔っ払い

--- python/dj_test/plac6/urls.py
+++ python/dj_test/plac6/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,master
 
 urlpatterns = [
     path('index/', index, name="index"),
+    path('master/', master, name="master"),
 ]
--- python/dj_test/plac6/views.py
+++ python/dj_test/plac6/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render,redirect
 import io
 import csv
 from django.http import HttpResponse
+from .models import Master
+
 
 def index(request):
     return render(request,"plac6/index.html")
+
+def master(request):
+    if 'csv' in request.FILES:
+        data = io.TextIOWrapper(request.FILES['csv'].file, encoding='utf-8')
+        csv_content = csv.reader(data)
+
+        for i in csv_content:
+            Master.objects.update_or_create(
+                jan = i[7],
+                defalults={
+                    "brand": i[0],
+                    "hinban": i[1],
+                    "hinmei": i[2],
+                    "color_no": i[3],
+                    "color_name": i[4],
+                    "size_no": i[5],
+                    "size_name": i[6],
+                    "jan": i[7],
+                }  
+            )
+        return redirect('index')
+    else:
+        return redirect('index')
 
 
 def csv_import(request):
@@ -13,11 +38,11 @@ def csv_import(request):
         print(data)
         csv_content = csv.reader(data)
         print(csv_content)
-        for i in csv_content:
-            hiragana, created = Hiragana.objects.get_or_create(moji = i[1])
-            hiragana.pk = i[0]
-            hiragana.moji = i[1]
-            hiragana.save()
+        # for i in csv_content:
+        #     hiragana, created = Hiragana.objects.get_or_create(moji = i[1])
+        #     hiragana.pk = i[0]
+        #     hiragana.moji = i[1]
+        #     hiragana.save()
         return redirect('index')
     else:
         return redirect('index')
