959be0ecb0174de25356e5475b2e94100ab38ae7
p1-kawamura <kawamura@p1-intl.com>
2022-03-08 17:59:33 +0900

    com1759

--- python/dj_test/plac7/views.py
+++ python/dj_test/plac7/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from plac6.models import Master
 
 def index(request):
     return render(request,"plac7/index.html")
@@ -8,10 +9,17 @@ def index(request):
 def ajax_number(request):
     number1 = int(request.POST.get('number1'))
     number2 = int(request.POST.get('number2'))
+    hinban = request.POST.get('hinban')
     plus = number1 + number2
     minus = number1 - number2
+    items= Master.objects.filter(hinban__contains = hinban).distinct().values_list("color_name",flat=True)
+    items=list(items)
+    # print(items)
     d = {
         'plus': plus,
         'minus': minus,
+        'hinban':hinban,
+        'items':items,
     }
+    # print(d)
     return JsonResponse(d)
