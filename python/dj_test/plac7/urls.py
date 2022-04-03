f91086a9a76f46c95de90c57edcf8b2e3a5ee7e3
kawamura <lailai666@hotmail.com>
2022-04-03 14:46:14 +0900

    desk1446

--- python/dj_test/plac7/urls.py
+++ python/dj_test/plac7/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import index,ajax_number
-from .tests import LoginTest
+
 
 app_name="plac7"
 urlpatterns = [
     path('index/', index, name="index"),
     path('ajax_number/', ajax_number ,name="ajax_number"),
-    path('LoginTest/', LoginTest,name="ajax_number"),
 ]
--- python/dj_test/plac7/views.py
+++ python/dj_test/plac7/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from plac6.models import Master
 from .forms import Plac7form
-# import urllib.request
-# from selenium import webdriver
-# import chromedriver_binary
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.options import Options
-# import requests
+
 
 def index(request):
     form=Plac7form()
@@ -31,10 +26,3 @@ def ajax_number(request):
     }
     # print(d)
     return JsonResponse(d)
-
-# options = Options()
-
-# def selenium_test(request):
-#     driver = webdriver.Chrome(options=options)
-#     driver.get("https://www.yahoo.co.jp/")
-#     return render(request,"plac7/index.html")
